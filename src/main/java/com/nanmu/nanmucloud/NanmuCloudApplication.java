a081b628126bccd1c071eb6f841423cb1e2f0112
chen.yu <361263670@qq.com>
2021-08-02 18:31:08 +0800

    Initial commit

---
+++ src/main/java/com/nanmu/nanmucloud/NanmuCloudApplication.java
@@ -0,0 +1,13 @@
+package com.nanmu.nanmucloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanmuCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanmuCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanmu/nanmucloud/NanmuCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanmu.nanmucloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanmuCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
